bb87517966942fc0f8ce5ec8eb4a52482866a336
danmarkpeas <gauravcrd@gmail.com>
2025-12-02 13:19:43 +0530

    added missing files

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import ProductViewer from "./components/ProductViewer";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Showcase from "./components/Showcase";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <main>
+      <NavBar />
+      <Hero />
+      <ProductViewer />
+      <Showcase />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
